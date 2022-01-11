e2c3d086407ca372296b4c396da3e45f2dc42a28
mightyMougli <mightymougli@gmail.com>
2022-01-11 13:19:27 +0200

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -9,7 +9,17 @@ def primary():
 
   last = len(quotes) - 1
   rnd = random.randint(0, last)
+  print(quotes[rnd], end = "")
+  rnd = random.randint(0, last)
   print(quotes[rnd])
+  
+  f = open("quotes.txt", "a")
+  f.write("\n" + "Random integer " + str(rnd) + " has been added to the collective")
+  f.close()
+  
+  f = open("quotes.txt", "r")
+  print(f.read())
+  
 
 if __name__== "__main__":
   primary()
